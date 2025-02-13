aeeb4c1cad1f0be2906ae83b93bd9bb79fc61c56
Yeison Muñoz <yeyeygameplays@gmail.com>
2025-02-13 07:28:14 -0500

    Class 6 (Yeison Munoz)

--- Assets/Sesiones/Yeison Muñoz/1 Character root motion/CharacterMovement.cs
+++ Assets/Sesiones/Yeison Muñoz/1 Character root motion/CharacterMovement.cs
@@ -8,11 +8,24 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private Camera camera;
     [SerializeField] private FloatDampener speedX;
     [SerializeField] private FloatDampener speedY;
+    [SerializeField] private float angularSpeed;
 
     private int speedXHash;
     private int speedYHash;
+    Quaternion targetRotation;
 
     private Animator animator;
+
+    private void SolveCharacterRotation()
+    {
+        Vector3 floorNormal = transform.up;
+        Vector3 cameraRealForward = camera.transform.forward;
+        float angleInterpolator = Mathf.Abs(Vector3.Dot(cameraRealForward, floorNormal));
+        Vector3 cameraForward = Vector3.Lerp(cameraRealForward, camera.transform.up, angleInterpolator).normalized;
+        Vector3 characterForward = Vector3.ProjectOnPlane(cameraForward, floorNormal).normalized;
+        Debug.DrawLine(transform.position, transform.position + cameraForward * 2, Color.magenta, 5);
+        targetRotation = Quaternion.LookRotation(characterForward, floorNormal);
+    }
 
     private void Awake()
     {
@@ -28,6 +41,8 @@ public class CharacterMovement : MonoBehaviour
 
         animator.SetFloat(speedXHash, speedX.CurrentValue);
         animator.SetFloat(speedYHash, speedY.CurrentValue);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, angularSpeed);
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
@@ -37,18 +52,9 @@ public class CharacterMovement : MonoBehaviour
         speedX.TargetValue = inputValue.x;
         speedY.TargetValue = inputValue.y;
 
-        //animator.SetFloat("SpeedX", inputValue.x);
-        //animator.SetFloat("SpeedY", inputValue.y);
-
-        Vector3 floorNormal = transform.up;
-        Vector3 cameraRealForward = camera.transform.forward;
-        float angleInterpolator = Mathf.Abs(Vector3.Dot(cameraRealForward, floorNormal));
-
-        Vector3 cameraForward = Vector3.Lerp(cameraRealForward, camera.transform.up, angleInterpolator).normalized;
-        
-        Vector3 characterForward = Vector3.ProjectOnPlane(cameraForward, floorNormal).normalized;
-
-        Debug.DrawLine(transform.position, transform.position + cameraForward * 2, Color.magenta, 5);
-
+        if(inputValue.magnitude > .1f)
+        {
+            SolveCharacterRotation();
+        }
     }
 }
