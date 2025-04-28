3a627c8ef5381e3c457ec8378539d990d571a239
Yeison Muñoz <yeyeygameplays@gmail.com>
2025-04-28 16:17:20 -0500

    lo que medio alcance a hacer

--- Assets/Sesiones/Yeison Muñoz/4 Combo Logic/Scripts/CharacterState.cs
+++ Assets/Sesiones/Yeison Muñoz/4 Combo Logic/Scripts/CharacterState.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 
+// summary
 //stores all dynamic variables for the character
+// summary
 public class CharacterState : MonoBehaviour
 {
     [SerializeField] private float startStamina;
     [SerializeField] private float staminaRegen;
 
-    private float currentStamina;
+    [SerializeField] private float currentStamina;
     private float currentHealth;
 
     private void RegenerateStamina(float regenAmount)
@@ -14,9 +16,15 @@ public class CharacterState : MonoBehaviour
         currentStamina = Mathf.Min(currentStamina + regenAmount, startStamina);
     }
 
+    private float GetStaminaDepletion()
+    {
+        //sistema de inventario *1/stat_fuerza * 1/buff_fuerza
+        return 60;
+    }
+
     public void DepleteStamina(float amount)
     {
-        currentStamina -= amount;
+        currentStamina -= GetStaminaDepletion() * amount;
     }
 
     private void Update()
